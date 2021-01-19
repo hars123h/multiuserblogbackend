e963595b1acffcd0ef7d7d49326d71ae9b961665
unknown <tripathihk2014@gmail.com>
2021-01-19 18:52:32 +0530

    Initial Commit

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const { 
+    signup, 
+    signin, 
+    signout, 
+    requireSignin, 
+    forgotPassword, 
+    resetPassword, 
+    preSignup 
+} = require('../controllers/auth');
+
+// validators
+const { runValidation } = require('../validators');
+const { 
+    userSignupValidator,
+    userSigninValidator,
+    forgotPasswordValidator,
+    resetPasswordValidator  } = require('../validators/auth');
+
+router.post('/pre-signup', userSignupValidator, runValidation, preSignup);
+router.post('/signup', signup);
+router.post('/signin', userSigninValidator, runValidation, signin);
+router.get('/signout', signout);
+router.put('/forgot-password', forgotPasswordValidator, runValidation, forgotPassword);
+router.put('/reset-password', resetPasswordValidator, runValidation, resetPassword);
+// Google Login 
+
+module.exports = router;
+
+// test
+// router.get('/secret', (req, res) => {
+//     res.json({
+//         user: req.user
+//     });
+// });
